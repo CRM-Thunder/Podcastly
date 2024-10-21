3214e83bfb6b06c843a15b0d828d65a4310ea444
CRM-Thunder <damian-zamecki@wp.pl>
2024-10-21 19:32:18 +0200

    first commit

---
+++ Backend/src/main/java/com/zamecki/Podcastly/PodcastlyApplication.java
@@ -0,0 +1,13 @@
+package com.zamecki.Podcastly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastlyApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/zamecki/Podcastly/PodcastlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamecki.Podcastly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
