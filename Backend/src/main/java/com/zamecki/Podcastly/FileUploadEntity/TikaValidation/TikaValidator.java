5ca80a0b6c7b684177bdc17937ab9b91aa06070d
CRM-Thunder <damian-zamecki@wp.pl>
2024-11-30 17:32:54 +0100

    fixed tikavalidator

--- Backend/src/main/java/com/zamecki/Podcastly/FileUploadEntity/TikaValidation/TikaValidator.java
+++ Backend/src/main/java/com/zamecki/Podcastly/FileUploadEntity/TikaValidation/TikaValidator.java
@@ -3,11 +3,13 @@ import org.apache.tika.Tika;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class TikaValidator {
     static public boolean isMp4File(MultipartFile file) throws IOException{
         Tika tika=new Tika();
         String detectedType=tika.detect(file.getInputStream());
-        return "video/mp4".equals(detectedType);
+        System.out.println(detectedType);
+        return Objects.equals(detectedType, "video/mp4") || Objects.equals(detectedType, "audio/mp4") || Objects.equals(detectedType, "video/quicktime");
     }
 }
